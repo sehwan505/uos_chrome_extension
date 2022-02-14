f501bf86fcc28fba146cdbe14880dc4a59fb3c98
sehwan505 <sehwan505@gmail.com>
2022-02-14 19:47:50 +0900

    add Server Clock

--- src/pages/Newtab/Newtab.jsx
+++ src/pages/Newtab/Newtab.jsx
@@ -4,25 +4,14 @@ import './Newtab.css';
 import './Newtab.scss';
 
 const Newtab = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Newtab/Newtab.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-        <h6>The color of this paragraph is defined using SASS.</h6>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+
+            </header>
+        </div>
+    );
 };
 
 export default Newtab;
--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import uos from './img/uos.png';
 import dormitory from './img/dormitory.png';
 import homepage from './img/homepage.png';
@@ -7,214 +7,165 @@ import schedule from './img/schedule.png';
 import notice from './img/noticeboard.png';
 import homework from './img/homework.png';
 import idea from './img/idea.png';
+import classroom from './img/classroom.png';
 import './Popup.css';
-import { withStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 const Popup = () => {
+    const [dropDown, setDropDown] = useState(false);
+    const [time, setTime] = useState(new Date());
+
+    const getServerTime = () => {
+        let xmlHttpRequest;
+        if (window.XMLHttpRequest) {// code for Firefox, Mozilla, IE7, etc.
+            xmlHttpRequest = new XMLHttpRequest();
+        } else if (window.ActiveXObject) {// code for IE5, IE6
+            xmlHttpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+        } else {
+            return;
+        }
+
+        xmlHttpRequest.open('HEAD', "https://wise.uos.ac.kr/", false);
+        xmlHttpRequest.setRequestHeader("ContentType", "text/html");
+        xmlHttpRequest.send('');
+
+        let serverDate = xmlHttpRequest.getResponseHeader("Date");
+        let date = new Date(serverDate);
+        setTime(date);
+    }
+
+    React.useEffect(() => {
+        let interval;
+        if (dropDown) {
+            interval = setInterval(() => {
+                getServerTime();
+            }, 20);
+        }
+        else {
+            clearInterval(interval);
+        }
+    }, [dropDown]);
+
     return (
-        <div className="App">
-            <div className='item'>
-                <a
-                    className="App-link"
-                    href="https://www.uos.ac.kr/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <img src={homepage} width="50px" height="50px" /><br />
-                </a>
-                홈페이지
+        <>
+            <div className="App">
+                <div className='item'>
+                    <a
+                        className="App-link"
+                        href="https://www.uos.ac.kr/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <img src={homepage} width="50px" height="50px" /><br />
+                    </a>
+                    홈페이지
+                </div>
+                <div className='item'>
+                    <a
+                        className="App-link"
+                        href="https://portal.uos.ac.kr/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <img src={uos} width="50px" height="50px" /><br />
+                    </a>
+                    포털
+                </div>
+                <div className='item'>
+                    <a
+                        className="App-link"
+                        href="https://uclass.uos.ac.kr/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <img src={classroom} width="50px" height="50px" /><br />
+                    </a>
+                    강의실
+                </div>
+                <div className='item'>
+                    <a
+                        className="App-link"
+                        href="https://dormitory.uos.ac.kr/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <img src={dormitory} width="50px" height="50px" /><br />
+                    </a>
+                    기숙사
+                </div>
+                <div className='item'>
+                    <a
+                        className="App-link"
+                        href="https://wise.uos.ac.kr/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <img src={idea} width="50px" height="50px" /><br />
+                    </a>
+                    WISE
+                </div>
+                <div className='item'>
+                    <a
+                        className="App-link"
+                        href="https://everytime.kr/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <img src={everytime} width="50px" height="50px" /><br />
+                    </a>
+                    에브리타임
+                </div>
+                <div className='item'>
+                    <a
+                        className="App-link"
+                        href="https://www.uos.ac.kr/korCalendarYear/list.do?list_id=CA1&epTicket=LOG"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <img src={schedule} width="50px" height="50px" /><br />
+                    </a>
+                    학사일정
+                </div>
+                <div className='item'>
+                    <a
+                        className="App-link"
+                        href="https://www.uos.ac.kr/korNotice/list.do?list_id=FA1&epTicket=LOG"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <img src={notice} width="50px" height="50px" /><br />
+                    </a>
+                    일반 공지
+                </div>
+                <div className='item'>
+                    <a
+                        className="App-link"
+                        href="https://www.uos.ac.kr/korNotice/list.do?list_id=FA2&epTicket=LOG"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <img src={homework} width="50px" height="50px" /><br />
+                    </a>
+                    학사 공지
+                </div>
+                {dropDown ?
+                    <div className='item' style={{ "cursor": "pointer" }} onClick={() => { setDropDown(false); }}>
+                        <KeyboardArrowUpIcon />
+                        <div className="item">
+                            <h1>WISE 서버시간</h1>
+                            <h1>
+                                {time.toLocaleString()}
+                            </h1>
+                        </div>
+                    </div>
+                    :
+                    <div className='item' style={{ "cursor": "pointer" }} onClick={() => { setDropDown(true); }}>
+                        <KeyboardArrowDownIcon />
+                    </div>
+                }
             </div>
-            <div className='item'>
-                <a
-                    className="App-link"
-                    href="https://portal.uos.ac.kr/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <img src={uos} width="50px" height="50px" /><br />
-                </a>
-                포털
-            </div>
-            <div className='item'>
-                <a
-                    className="App-link"
-                    href="https://dormitory.uos.ac.kr/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <img src={dormitory} width="50px" height="50px" /><br />
-                </a>
-                기숙사
-            </div>
-            <div className='item'>
-                <a
-                    className="App-link"
-                    href="https://everytime.kr/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <img src={everytime} width="50px" height="50px" /><br />
-                </a>
-                에브리타임
-            </div>
-            <div className='item'>
-                <a
-                    className="App-link"
-                    href="https://wise.uos.ac.kr/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <img src={idea} width="50px" height="50px" /><br />
-                </a>
-                WISE
-            </div>
-            <div className='item'>
-                <a
-                    className="App-link"
-                    href="https://www.uos.ac.kr/korCalendarYear/list.do?list_id=CA1&epTicket=LOG"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <img src={schedule} width="50px" height="50px" /><br />
-                </a>
-                학사일정
-            </div>
-            <div className='item'>
-                <a
-                    className="App-link"
-                    href="https://www.uos.ac.kr/korNotice/list.do?list_id=FA1&epTicket=LOG"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <img src={notice} width="50px" height="50px" /><br />
-                </a>
-                일반 공지
-            </div>
-            <div className='item'>
-                <a
-                    className="App-link"
-                    href="https://www.uos.ac.kr/korNotice/list.do?list_id=FA2&epTicket=LOG"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <img src={homework} width="50px" height="50px" /><br />
-                </a>
-                학사 공지
-            </div>
-        </div>
+        </>
     );
 };
 
 export default Popup;
-
-
-{/*<div className={classes.root}>
-<Grid container spacing={3}>
-  <Grid item xs={4}>
-    <Paper className={classes.paper}>
-            <a
-                className="App-link"
-                href="https://www.uos.ac.kr/"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                <img src={uos} width="50px" height="50px" /><br />
-                홈페이지
-            </a>
-    </Paper>
-  </Grid>
-  <Grid item xs={4}>
-    <Paper className={classes.paper}>
-            <a
-                className="App-link"
-                href="https://portal.uos.ac.kr/"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                <img src={uos} width="50px" height="50px" /><br />
-                포털
-            </a>
-    </Paper>
-  </Grid>
-  <Grid item xs={4}>
-    <Paper className={classes.paper}>
-            <a
-                className="App-link"
-                href="https://dormitory.uos.ac.kr/"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                <img src={uos} width="50px" height="50px" /><br />
-                기숙사
-            </a>
-            </Paper>
-  </Grid>
-  <Grid item xs={4}>
-    <Paper className={classes.paper}>
-            <a
-                className="App-link"
-                href="https://everytime.kr/"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                <img src={uos} width="50px" height="50px" /><br />
-                에브리타임
-            </a>
-    </Paper>
-  </Grid>
-  <Grid item xs={4}>
-    <Paper className={classes.paper}>
-        <a
-            className="App-link"
-            href="https://uostime.com"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-            <img src={uos} width="50px" height="50px" /><br />
-            UOS Time
-        </a>
-    </Paper>
-  </Grid>
-  <Grid item xs={4}>
-    <Paper className={classes.paper}>
-            <a
-                className="App-link"
-                href="https://www.uos.ac.kr/korCalendarYear/list.do?list_id=CA1&epTicket=LOG"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                <img src={uos} width="50px" height="50px" /><br />
-                학사일정
-            </a>
-    </Paper>
-  </Grid>
-  <Grid item xs={4}>
-    <Paper className={classes.paper}>
-            <a
-                className="App-link"
-                href="https://www.uos.ac.kr/korNotice/list.do?list_id=FA1&epTicket=LOG"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                <img src={uos} width="50px" height="50px" /><br />
-                일반 공지
-            </a>
-    </Paper>
-  </Grid>
-  <Grid item xs={4}>
-    <Paper className={classes.paper}>
-    <a
-            className="App-link"
-            href="https://www.uos.ac.kr/korNotice/list.do?list_id=FA2&epTicket=LOG"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-            <img src={uos} width="50px" height="50px" /><br />
-            학사 공지
-        </a>
-    </Paper>
-  </Grid>
-</Grid>
-</div>*/}
