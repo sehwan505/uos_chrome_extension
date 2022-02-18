0d66b522a735314b4b8479833eaa8aea080475c7
sehwan505 <sehwan505@gmail.com>
2022-02-18 14:09:20 +0900

    delete newtab page

--- src/pages/Options/Options.tsx
+++ src/pages/Options/Options.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import './Options.css';
 
 interface Props {
-  title: string;
+    title: string;
 }
 
 const Options: React.FC<Props> = ({ title }: Props) => {
-  return <div className="OptionsContainer">{title} Page</div>;
+    return <div className="OptionsContainer">{title} Page</div>;
 };
 
 export default Options;
